cbd76d6d362b3c443598caa32ddf07d15b412c31
mutian <bhfqlz@qq.com>
2023-01-03 20:28:32 +0800

    notices:
    version 2.2.24

--- src/config/DefaultApplicationConfig.php
+++ src/config/DefaultApplicationConfig.php
@@ -5,8 +5,9 @@ namespace muyomu\framework\config;
 use muyomu\auth\MuixAuthMiddleWare;
 use muyomu\config\annotation\Configuration;
 use muyomu\config\GenericConfig;
+use muyomu\dashboard\identifier\ApplicationConfig;
 
-#[Configuration("config_application")]
+#[Configuration(ApplicationConfig::class)]
 class DefaultApplicationConfig extends GenericConfig
 {
     protected string $configClass = self::class;
